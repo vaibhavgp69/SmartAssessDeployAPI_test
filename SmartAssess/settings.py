f7cc6ddedd30aea4240f6fc1c68511b36be32e03
Vaibhav GP <112196928+vaibhavgp69@users.noreply.github.com>
2024-02-01 12:48:12 +0530

    Update settings.py

--- SmartAssess/settings.py
+++ SmartAssess/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-g9ke(4clu+xnau+-ib^psq#1b3)g!+7i16(ugfu6p=b0bj&5dn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
